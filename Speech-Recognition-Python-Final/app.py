0d3d37c2a2389cf706f9669cf106aa5ea8781345
Avinash Jain <avinashkalpitjain@gmail.com>
2020-07-28 14:51:45 -0700

    Added Speech Recognition Project

---
+++ Speech-Recognition-Python-Final/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect
+import speech_recognition as sr
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    transcript = ""
+    if request.method == "POST":
+        print("FORM DATA RECEIVED")
+
+        if "file" not in request.files:
+            return redirect(request.url)
+
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+
+        if file:
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile(file)
+            with audioFile as source:
+                data = recognizer.record(source)
+            transcript = recognizer.recognize_google(data, key=None)
+
+    return render_template('index.html', transcript=transcript)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
